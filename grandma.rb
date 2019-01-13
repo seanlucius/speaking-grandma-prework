e3fcb2743d66c476db21972331583b00831d50ae
Sean Lucius <seanlucius22@icloud.com>
2019-01-13 02:23:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,6 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
